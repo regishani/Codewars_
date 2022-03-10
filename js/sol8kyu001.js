6b250348aa2a95319eb6331819fd5cba322a5085
Regina Zhyldyzbekova <regina.zhyldyzbekova@gmail.com>
2022-03-10 15:13:28 -0500

    Add coding challenge for 10-03-22

--- js/sol8kyu001.js
+++ js/sol8kyu001.js
@@ -1,36 +1,4 @@
-// Given an array of integers.
-
-// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
-
-// If the input is an empty array or is null, return an empty array.
-
-function countPositivesSumNegatives(input) {
-    var newArr = [];
-    var positiveNumber = 0;
-    var negativeNumber = 0;
-    
-    // Validate Input
-    if (input === null || input.length === 0)
-      return newArr;
-    
-    // Loop through array of Numbers 
-    for (var i = 0; i < input.length; i++) {
-      if (input[i] == 0)
-       continue;
-    
-      // Count positives
-      else if (input[i] > 0) 
-        positiveNumber++;     
-          
-      // Sum negatives
-      else if (input[i] < 0) 
-        negativeNumber += input[i];
-      
-    }
-    
-    // Prepare Output
-    newArr.push(positiveNumber);
-    newArr.push(negativeNumber);
-    
-    return newArr;
-  }
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+function binToDec(bin){
+  return parseInt(bin, 2)
+}
