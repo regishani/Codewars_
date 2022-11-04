0161127ee7ab7a06ec208b8c19c0f8a829b629c7
Regina Zhyldyzbekova <regina.zhyldyzbekova@gmail.com>
2022-11-04 16:23:36 +0600

    add coding challenge for 04/11/22

---
+++ js/reverse04.js
@@ -0,0 +1,5 @@
+//reverse a string in js
+ let str = "Hello world"
+ let reversed = str.split("").reverse().join("") //split () method splits into an array of individual character and reverse method reverse the order of the character and lastly the join method joins all of the characters into string
+
+ console.log(reversed)
